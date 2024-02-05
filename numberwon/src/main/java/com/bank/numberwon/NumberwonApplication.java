d85bc1c9f6ada5c4ad4f0647fe0a7f94d9cfcc51
sws6641 <sws6641@naver.com>
2024-02-05 12:10:14 +0900

    feat: 프로젝트 초기 세팅

---
+++ numberwon/src/main/java/com/bank/numberwon/NumberwonApplication.java
@@ -0,0 +1,13 @@
+package com.bank.numberwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberwonApplication.class, args);
+	}
+
+}
